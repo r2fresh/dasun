537f5e253fed89fae5b2c970e9e9d8457cd08e8a
r2fresh <r2fresh78@gmail.com>
2017-02-14 17:04:49 +0900

    timeline 기본적인 개발 기능 완료

--- src/js/Timeline.js
+++ src/js/Timeline.js
@@ -15,8 +15,31 @@ define([
  		el: '.d-timeline',
         prevView: null,
         timeLineListTpl:'',
-        initialize:function(){
+        timelineData : null,
+        timelineKeywordTpl : '',
+        timeline : null,
+        events :{
+            'click #visualization': 'onItemClick'
+ 		},
+        onItemClick:function(e){
+            var props = this.timeline.getEventProperties(e)
+            console.log(props);
 
+            if(props.item == null) return;
+
+            var news_data = _.find(this.timelineData,function(item){
+                return item.id === props.item
+            })
+
+            this.setNews(news_data)
+        },
+        setNews:function(item){
+            console.log(item.keywords)
+            var template = Handlebars.compile(this.timelineKeywordTpl);
+            this.$el.find('.d-timeline-keyword-list').html(template({'keywords':item.keywords}));
+        },
+        initialize:function(){
+            console.log( new Date(moment('2017-02-14 16:00:00')) )
         },
         render:function(mainMenu){
 
@@ -29,11 +52,11 @@ define([
 
             //this.timeLineListTpl     = $(Template).find('.d-timeline-tpl').html();
             this.timeLineListTpl2     = $(Template).find('.d-timeline2-tpl').html();
+            this.timelineKeywordTpl = $(Template).find('.d-timeline-keyword-tpl').html();
 
 
-            //this.getKeyword();
-            // this.setTimeline();
-            this.setTimeline2();
+            this.getKeyword();
+            // this.setTimeline2();
         },
         getKeyword : function(){
             Model.getKeyword({
@@ -45,10 +68,63 @@ define([
             //var template = Handlebars.compile(this.timeLineListTpl);
             //this.$el.find('.timeline-list').html(template({'list':data}));
             //this.setTimeline();
+            this.setDataModify(data)
+
         },
         getKeywordError : function(jsXHR, textStatus, errorThrown){
 
         },
+
+        setDataModify : function(data){
+
+            var modify_data = _.map(data,function(item){
+
+                    _.each(item.data,function(data_item){
+                        data_item.created_at = item.created_at;
+                    })
+
+                return item
+            })
+
+            this.timelineData = [];
+
+            for(var i=0; i<modify_data.length; i++) {
+
+                for(var j=0; j<modify_data[i].data.length; ++j) {
+
+                    var obj = {
+                        start : new Date(modify_data[i].created_at)
+                    }
+
+                    obj.content = '<div class="title" data-index="0">' + modify_data[i].data[j].candidate + '</div>';
+
+                    var keyword = ''
+
+                    for(var k=0; k<modify_data[i].data[j].keywords.length; ++k) {
+
+                        keyword += modify_data[i].data[j].keywords[k].keyword;
+
+                    }
+
+                    obj.content += '<div>' + keyword + '</div>';
+
+                    obj.keywords = modify_data[i].data[j].keywords
+
+                    this.timelineData.push(obj);
+
+                    obj = null;
+                }
+
+
+
+                console.log(this.timelineData)
+            }
+
+            //console.log(reData)
+            this.setTimeline();
+        },
+
+
         setTimeline2 : function(){
             var template = Handlebars.compile(this.timeLineListTpl2);
             //this.$el.find('.timeline-list').html(template({'list':data}));
@@ -146,7 +222,27 @@ define([
         },
         setTimeline : function(){
 
+
+            var ktest = [
+                {id:'1',content:'aaa'},
+                {id:'2',content:'bbb'},
+                {id:'3',content:'ccc'}
+            ]
+
+            console.log(_.filter(ktest,function(item){
+                return item.id === '1';
+            }))
+
+
             var container = document.getElementById('visualization');
+
+
+            // console.log(this.timelineData)
+            // console.log(sss)
+            // console.log(sss[sss.length-1].start)
+            //
+            console.log(moment(this.timelineData[this.timelineData.length-1].start))
+            console.log(new Date( moment(this.timelineData[this.timelineData.length-1].start).add(2,'hours')) )
 
             var kkk = [
               {start: new Date(2010,8,4,9,0,0), content: '<div class="title" data-index="0">문재인</div><div>김동근,이민향,송민정</div>'},
@@ -160,7 +256,7 @@ define([
               {start: new Date(2010,8,4,15,0,0), content: '<div class="title" data-index="7">안희정</div><div>김동근,이민향,송민정</div>'}
             ]
             // note that months are zero-based in the JavaScript Date object
-            var items = new vis.DataSet(kkk);
+            var items = new vis.DataSet(this.timelineData);
 
             var options = {
               editable: true,
@@ -168,13 +264,14 @@ define([
                 item: 20,
                 axis: 40
             },
+             //timeAxis: {scale: 'hour', step: 3},
             // timeAxis : {
             //     scale: 'day',
             //     step : 10
             // },
                 zoomable : false,
-                min : new Date(2010,8,4,6,0,0),
-                max : new Date(2010,8,4,21,0,0),
+                min : new Date( moment(this.timelineData[this.timelineData.length-1].start).subtract(1,'hours')),
+                max : new Date( moment(this.timelineData[0].start).add(1,'hours')),
 
                 editable : {
                     remove : false
@@ -182,14 +279,17 @@ define([
 
             };
 
-            var timeline = new vis.Timeline(container, items, options);
-             timeline.zoomIn(0.8);
+            this.timeline = new vis.Timeline(container, items, options);
+             this.timeline.zoomIn(0.8);
+             this.timeline.moveTo(new Date( moment(this.timelineData[0].start).add(1,'hours')))
 
-             timeline.on('rangechanged',function(e){
-                 console.log(moment(e.start).hour())
+             console.log(this.timeline)
+
+             this.timeline.on('rangechanged',function(e){
+                 console.log(moment(e.start).format('HH:mm'))
                  console.log("sdfdsfs")
 
-                 if(moment(e.start).hour() <= 6){
+                 if(moment(e.start).format('HH:mm') == moment( options.min ).format('HH:mm') ){
                      $('.d-timeline-more').removeClass('displayNone')
                      $('.d-timeline-body').removeClass('col-xs-12').addClass('col-xs-11')
                  } else {
@@ -233,21 +333,41 @@ define([
                // adjust the timeline window such that we see the loaded data
                //timeline.fit();
 
-               timeline.setOptions({
+               this.timeline.setOptions({
                    min : new Date(2010,8,4,1,0,0),
                })
              })
 
-             console.log(this.$el.find('.vis-group').html())
+            //  this.$el.find('#visualization').on('click',function(e){
+            //      var props = timeline.getEventProperties(e)
+            //      console.log(props);
+             //
+            //      _.find(sss,function(item){
+            //          return item.id === props.item
+            //      })
+            //  })
 
-                this.$el.find('.vis-group').bind('click',function(e){
-                    console.log($(this).find('.vis-item-content').html())
-                e.preventDefault();
-                 console.log("1234")
-                 //console.log($(e.target).data('index'))
-                 //console.log(kkk[$(e.target).find('.title').data('index')])
-                 //$('.d-timeline-keyword-list').html(JSON.stringify(kkk[$(e.currentTarget).find('.title').data('index')]));
-             })
+            //  document.getElementById('visualization').onclick = function (event) {
+            //      var props = timeline.getEventProperties(event)
+            //      console.log(props);
+             //
+            //      _.find(sss,function(item){
+            //          return item.id === props.item
+            //      })
+             //
+            //      //items.getCustomTime(props.)
+            //    }
+
+            //  console.log(this.$el.find('.vis-item-content').find('.title').text())
+             //
+            //     this.$el.find('.vis-item-content').bind('click',function(e){
+            //         //console.log($(this).find('.vis-item-content').html())
+            //         //e.preventDefault();
+            //      console.log("1234")
+            //      //console.log($(e.target).data('index'))
+            //      //console.log(kkk[$(e.target).find('.title').data('index')])
+            //      //$('.d-timeline-keyword-list').html(JSON.stringify(kkk[$(e.currentTarget).find('.title').data('index')]));
+            //  })
         },
         hide : function(){
             this.$el.addClass('displayNone');
